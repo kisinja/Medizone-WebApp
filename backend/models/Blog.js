ad0af86766789e9be448e923869a4ab431abb193
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-12-08 18:32:08 +0300

    backend fix

--- backend/models/Blog.js
+++ backend/models/Blog.js
@@ -13,7 +13,6 @@ const blogSchema = new mongoose.Schema(
         },
         author: {
             type: String,
-            required: true,
             default: "Admin",
         },
         imageUrls: [{
