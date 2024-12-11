02539b8d32cb90a764832c48aa741f46230e6cee
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-12-11 17:58:17 +0300

    c

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,20 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import About from "./pages/About"
-import Home from "./pages/Home"
-import Contact from "./pages/Contact"
-import Doctors from "./pages/Doctors"
-import MyAppointments from "./pages/MyAppointments"
-import Profile from "./pages/Profile"
-import Login from "./pages/Login"
-import Appointment from "./pages/Appointment"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
+import { Navigate, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Doctors from "./pages/Doctors";
+import MyAppointments from "./pages/MyAppointments";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import PrivacyPolicy from "./pages/PrivacyPolicy"
-import TermsAndConditions from "./pages/TermsAndConditions"
-import JobOpenings from "./pages/JobOpenings"
-import { useContext } from "react"
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsAndConditions from "./pages/TermsAndConditions";
+import JobOpenings from "./pages/JobOpenings";
+import { useContext } from "react";
 import { AppContext } from "./context/AppContext";
 import Blogs from "./pages/Blogs";
 import BlogDetails from "./pages/BlogDetails";
@@ -29,19 +29,33 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/about" element={<About />} />
+
         <Route path="/contact" element={<Contact />} />
+
         <Route path="/doctors/:speciality" element={token ? <Doctors /> : <Navigate to="/login" />} />
+
         <Route path="/doctors/" element={token ? <Doctors /> : <Navigate to="/login" />} />
+
         <Route path="/blogs" element={token ? <Blogs /> : <Navigate to="/login" />} />
+
         <Route path="/blogs/:id" element={token ? <BlogDetails /> : <Navigate to="/login" />} />
+
         <Route path="/my-profile" element={token ? <Profile /> : <Navigate to="/login" />} />
+
         <Route path="/my-appointments" element={token ? <MyAppointments /> : <Navigate to="/login" />} />
+
         <Route path="/appointment/:docId" element={token ? <Appointment /> : <Navigate to="/login" />} />
+
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+
         <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+
         <Route path="/job-openings" element={<JobOpenings />} />
+
         <Route path="/login" element={token ? <Navigate to="/" /> : <Login />} />
+
       </Routes>
       <Footer />
     </div>
